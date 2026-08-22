e8452ddcc29e87f0e989a93cdbc995af478e7c75
Winston T. <65579453+TWinston-66@users.noreply.github.com>
2026-08-21 22:18:15 -0600

    linter updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,5 +16,20 @@ return {
                 "bashls",
             },
         })
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(event)
+                local opts = { buffer = event.buf }
+                local keymap = vim.keymap
+
+                keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+            end,
+        })
     end,
 }
--- nvim/.config/nvim/lua/plugins/syntax.lua
+++ nvim/.config/nvim/lua/plugins/syntax.lua
@@ -4,10 +4,27 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        local langs = { 'lua', 'vim', 'vimdoc', 'go', 'javascript', 'c', 'c++', 'rust', 'bash' }
+        -- vim filetype -> treesitter parser name, where they differ
+        local parser_by_filetype = {
+            help = 'vimdoc',
+            sh = 'bash',
+            javascriptreact = 'javascript',
+            typescriptreact = 'tsx',
+        }
+        local filetypes = {
+            'lua', 'vim', 'help', 'go', 'c', 'cpp', 'rust', 'sh',
+            'javascript', 'javascriptreact', 'typescript', 'typescriptreact',
+        }
+
+        local parsers = {}
+        for _, ft in ipairs(filetypes) do
+            parsers[parser_by_filetype[ft] or ft] = true
+        end
+        local langs = vim.tbl_keys(parsers)
         require('nvim-treesitter').install(langs)
+
         vim.api.nvim_create_autocmd('FileType', {
-            pattern = langs,
+            pattern = filetypes,
             callback = function() vim.treesitter.start() end,
         })
     end,

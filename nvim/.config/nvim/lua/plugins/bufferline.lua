4f5e43b6343a4e7a74d89d2bc78cffe0279feaee
Winston T. <65579453+TWinston-66@users.noreply.github.com>
2026-08-22 00:17:58 -0600

    key mapping fixes

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -36,11 +36,6 @@ return {
         event = "VeryLazy",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         keys = {
-            { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
-            { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",  desc = "Delete Buffers to the Left" },
-            { "<leader>bj", "<cmd>BufferLinePick<cr>",       desc = "Pick Buffer" },
-            { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",  desc = "Prev Buffer" },
-            { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",  desc = "Next Buffer" },
             { "[b",         "<cmd>BufferLineCyclePrev<cr>",  desc = "Prev Buffer" },
             { "]b",         "<cmd>BufferLineCycleNext<cr>",  desc = "Next Buffer" },
             { "[B",         "<cmd>BufferLineMovePrev<cr>",   desc = "Move Buffer Prev" },
--- nvim/.config/nvim/lua/plugins/scroll.lua
+++ nvim/.config/nvim/lua/plugins/scroll.lua
@@ -2,12 +2,6 @@ return {
     "karb94/neoscroll.nvim",
     config = function()
         require('neoscroll').setup({
-            mappings = {
-                '<C-u>', '<C-d>',
-                '<C-b>', '<C-f>',
-                '<C-y>', '<C-e>',
-                'zt', 'zz', 'zb',
-            },
             hide_cursor = true,
             stop_eof = true,
             respect_scrolloff = false,

9e7a1c1387dc5066ee5d4088d0b09f307806c314
Winston T. <65579453+TWinston-66@users.noreply.github.com>
2026-08-22 02:11:29 -0600

    lsp tweaks

--- nvim/.config/nvim/lua/plugins/code/lsp.lua
+++ nvim/.config/nvim/lua/plugins/code/lsp.lua
@@ -1,13 +1,12 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        require("mason").setup()
-
-        require("mason-lspconfig").setup({
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+            "saghen/blink.cmp",
+        },
+        opts = {
             ensure_installed = {
                 "gopls",
                 "clangd",
@@ -16,21 +15,43 @@ return {
                 "bashls",
                 "lua_ls",
             },
-        })
+        },
+        config = function(_, opts)
+            require("mason").setup()
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(event)
-                local opts = { buffer = event.buf }
-                local keymap = vim.keymap
+            vim.lsp.config("*", {
+                capabilities = require("blink.cmp").get_lsp_capabilities(),
+            })
 
-                keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-            end,
-        })
-    end,
+            require("mason-lspconfig").setup(opts)
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local keymap_opts = { buffer = event.buf }
+                    local keymap = vim.keymap
+
+                    keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+                    keymap.set("n", "gD", vim.lsp.buf.declaration, keymap_opts)
+                    keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+                    keymap.set("n", "gi", vim.lsp.buf.implementation, keymap_opts)
+                    keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+                    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, keymap_opts)
+                    keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, keymap_opts)
+                end,
+            })
+        end,
+    },
+
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "*",
+        opts = {
+            keymap = { preset = "default" },
+            appearance = { nerd_font_variant = "mono" },
+            completion = { documentation = { auto_show = true } },
+            sources = { default = { "lsp", "path", "snippets", "buffer" } },
+        },
+        opts_extend = { "sources.default" },
+    },
 }
